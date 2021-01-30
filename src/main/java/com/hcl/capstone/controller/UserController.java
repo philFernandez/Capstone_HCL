ab7e33cb854b91261841f6b46b377b052830347c
Phil Fernandez <norcal618@gmail.com>
2021-01-30 13:51:55 -0800

    fix deps

--- src/main/java/com/hcl/capstone/controller/UserController.java
+++ src/main/java/com/hcl/capstone/controller/UserController.java
@@ -1,9 +1,23 @@
 package com.hcl.capstone.controller;
 
+import org.apache.tomcat.jni.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    // @GetMapping("/register")
+    // public ModelAndView register() {
+    // return new ModelAndView("register", "user", new User());
+    // }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
 }
