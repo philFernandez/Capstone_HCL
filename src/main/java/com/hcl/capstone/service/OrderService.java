14456c9f0a41ae957224ffb8b03976ce444f0bec
Phil Fernandez <norcal618@gmail.com>
2021-02-01 21:45:17 -0800

    orderService

---
+++ src/main/java/com/hcl/capstone/service/OrderService.java
@@ -0,0 +1,24 @@
+package com.hcl.capstone.service;
+
+import java.util.List;
+import com.hcl.capstone.model.Order;
+import com.hcl.capstone.model.OrderRepository;
+import com.hcl.capstone.model.Product;
+import com.hcl.capstone.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository repository;
+
+    public void addProducts(Order order, List<Product> products) {
+        order.setProducts(products);
+        repository.save(order);
+    }
+
+    public void addUserToOrder(Order order, User user) {
+        order.setUser(user);
+    }
+}
